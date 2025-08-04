306f9d85a00cf6521840c65881f74590e27d2f84
mdzakied <mdzakied123@gmail.com>
2025-08-05 05:44:51 +0700

    fix home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-4xl text-white font-bold">Tailwind Aktif</h1>
-    </div>
-  );
+  redirect("/auth/login");
 }
